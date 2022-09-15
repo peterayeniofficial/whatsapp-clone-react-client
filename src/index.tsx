fa8d4880684985950070c7cecb2861fff80791b4
Peter Ayeni <ayenicology@gmail.com>
2022-09-15 10:10:23 +0100

    chore: configure material ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#2c6157' },
+    secondary: { main: '#6fd056' },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <MuiThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </MuiThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
